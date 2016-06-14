3984ec3eb7595ff8575a0bb7b64bbd3e97a84f95
The Enigma <j4nusx@yahoo.com>
2016-06-13 22:01:36 -0500

    Added comments to functions of http_client.hpp
    Nothing else to say :|
    
    Signed-off-by: The Enigma <j4nusx@yahoo.com>

--- headers/tcp/http_client.hpp
+++ headers/tcp/http_client.hpp
@@ -22,9 +22,32 @@ struct server_reply
     size_t length;
 };
 
+//!----------------------------------------------------------------------
+//!
+//! \brief
+//!    Perform a http post with a json string.
+//!
+//! \param [out]
+//!     sr_ptr      Double pointer where server_reply is allocated
+//!                 and builded.
+//! \return
+//!     int
+//!
 extern int
 post_json_data( const char* url_ptr , char* data_ptr , size_t data_length , struct server_reply** sr_ptr );
 
+
+//!----------------------------------------------------------------------
+//!
+//! \brief
+//!    Free memory utilized by a server_reply.
+//!
+//! \param [in]
+//!     sr_ptr      server_reply to be free.
+//!                 
+//! \return
+//!     void
+//!
 extern void
 server_reply_cleanup( struct server_reply* sr_ptr );
 
