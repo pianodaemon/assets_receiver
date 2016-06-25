d99474f030729786cd270d3fdb05fe77483a7de8
The Enigma <j4nusx@yahoo.com>
2016-06-25 15:40:20 -0500

    Added error code header for tcp and util stuff
    We have forgotten to add so. :p
    
    Signed-off-by: The Enigma <j4nusx@yahoo.com>

---
+++ headers/tcp/error_codes.hpp
@@ -0,0 +1,23 @@
+//======================================================================
+//  Company:
+//      Maxima, Software
+//
+//  Product:
+//      Asset Receiver for Agnux ERP
+//      Copyright 2016
+//
+//  \brief
+//       Tcp Stuff errors header
+//
+//======================================================================
+
+#ifndef __TCP_STUFF_ERRORS_HPP__
+#define __TCP_STUFF_ERRORS_HPP__
+
+#define TCP_STUFF_SUCCEED             0
+#define TCP_STUFF_SYSTEM_ERROR       -1
+
+#define TCP_STUFF_ARG_NULL_PTR       -3000
+#define TCP_STUFF_CURL_FUCK_US       -3001
+
+#endif /* __TCP_STUFF_ERRORS_HPP__ */
---
+++ headers/util/error_codes.hpp
@@ -0,0 +1,24 @@
+//======================================================================
+//  Company:
+//      Maxima, Software
+//
+//  Product:
+//      Asset Receiver for Agnux ERP
+//      Copyright 2016
+//
+//  \brief
+//       Util Helpers errors header
+//
+//======================================================================
+
+#ifndef __UTIL_STUFF_ERRORS_HPP__
+#define __UTIL_STUFF_ERRORS_HPP__
+
+#define UTIL_SUCCEED                  0
+#define UTIL_SYSTEM_ERROR           -1
+
+#define UTIL_DIR_IS_ALREADY_FILE     -6000
+#define UTIL_DIR_WASNT_CREATED       -6001
+#define UTIL_DIR_ALREADY_EXISTS      -6002
+
+#endif /* __UTIL_STUFF_ERRORS_HPP__ */
